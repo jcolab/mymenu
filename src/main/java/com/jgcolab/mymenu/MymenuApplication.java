70e068b4bd9e0c980183c7b89613c846009db288
Rafael <rafael.goomes91@gmail.com>
2021-09-25 10:48:00 -0300

    First Commit

---
+++ src/main/java/com/jgcolab/mymenu/MymenuApplication.java
@@ -0,0 +1,13 @@
+package com.jgcolab.mymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgcolab/mymenu/MymenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgcolab.mymenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
